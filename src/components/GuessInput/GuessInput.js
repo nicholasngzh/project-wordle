be7a3501dc49ed4333a099e6f2cacf08f069dfe4
Nicholas Ng <nickng-1998@hotmail.com>
2024-05-14 20:26:24 +0800

    implement guess history

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -2,27 +2,45 @@ import React from "react"
 
 function GuessInput() {
     const [guess, setGuess] = React.useState("")
+    const [guessHistory, setGuessHistory] = React.useState([])
     const handleSubmitGuess = (event) => {
         event.preventDefault()
         // do something here
         console.log(guess)
+
+        const nextGuessHistory = [...guessHistory]
+        nextGuessHistory.push({
+            value: guess,
+            key: crypto.randomUUID(),
+        })
+        setGuessHistory(nextGuessHistory)
+
         setGuess("")
     }
 
     return (
-        <form class="guess-input-wrapper" onSubmit={handleSubmitGuess}>
-            <label for="guess-input">Enter guess:</label>
-            <input
-                id="guess-input"
-                type="text"
-                value={guess}
-                pattern="[a-zA-Z]{5}"
-                title="5 letter word"
-                onChange={(event) => {
-                    setGuess(event.target.value.toUpperCase())
-                }}
-            />
-        </form>
+        <>
+            <div class="guess-results">
+                {guessHistory.map((guess) => (
+                    <p class="guess" key={guess.key}>
+                        {guess.value}
+                    </p>
+                ))}
+            </div>
+            <form class="guess-input-wrapper" onSubmit={handleSubmitGuess}>
+                <label for="guess-input">Enter guess:</label>
+                <input
+                    id="guess-input"
+                    type="text"
+                    value={guess}
+                    pattern="[a-zA-Z]{5}"
+                    title="5 letter word"
+                    onChange={(event) => {
+                        setGuess(event.target.value.toUpperCase())
+                    }}
+                />
+            </form>
+        </>
     )
 }
 
